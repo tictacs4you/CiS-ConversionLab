953781980d9d0ceeab9686c0f2b122f001edc619
Hurk_Burlap <biglolostyle@gmail.com>
2022-02-27 14:43:41 -0800

    Add User input Processing
    Add Case 2 for Celsius to Farenheit
    Add Case 3 for exiting program
    Add continuous loop

--- src/testPack/ConverF2C.java
+++
@@ -1,8 +0,0 @@
-package testPack;
-
-public class ConverF2C {
-	public static void F2C() {
-		System.out.println("Converting from Farenheit to Celcius");
-		}
-	}
-	
--- src/testPack/test.java
+++ src/testPack/test.java
@@ -6,26 +6,41 @@ public class test {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean continueProgram = true;
 		
-		
-		System.out.println("Hello user, and welcome to the Celsius and Farenheit converter by Logan and Kenny.  To begin, please select one of the options below.");
-		System.out.println("1: Convert Farenheit to Celsius");
-		System.out.println("2: Convert Celsius to Farenheit");
-		
-		Scanner in = new Scanner(System.in);
-		int option = in.nextInt();
-		switch(option) {
-			case 1:
-				F2C();
-				break;
-				
+		System.out.println("Hello user, and welcome to the Celsius and Farenheit converter by Logan and Kenny.");
+		while (true == continueProgram) {
+			System.out.println("To begin, please select one of the options below.");
+			System.out.println("1: Convert Farenheit to Celsius");
+			System.out.println("2: Convert Celsius to Farenheit");
+			System.out.println("3: Exit Program");
+			
+			Scanner in = new Scanner(System.in);
+			int option = in.nextInt();
+			switch(option) {
+				case 1:
+					F2C();
+					break;
+				case 2:
+					C2F();
+					break;
+				case 3:
+					continueProgram = false;
+					break;
+				default:
+					System.out.println("That is not a valid input, please type '1' or '2' to make your selection.");
+					
+			}
 		}
 		
 	}
 
 	private static void F2C() {
 		// TODO Auto-generated method stub
-		System.out.print("Converting farenheit to Celcius");
+		System.out.println("This works");
+	}
+	private static void C2F() {
+		System.out.println("This works");
 	}
 
 }
